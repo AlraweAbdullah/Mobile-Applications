6f9e6f846745866b8fc54c8fb9eb2f9d51cec2e7
AlraweAbdullah <abdullah.alrawe@student.ucll.be>
2023-11-13 04:46:28 +0100

    initial project requirements

---
+++ client/navigation.js
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
+import CartScreen from './screens/CartScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={
+        { headerShown: false }
+      }>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        <Stack.Screen name="Cart" options={{ presentation: 'modal', headerShown: false }} component={CartScreen} />
+        <Stack.Screen name="PreparingOrder" options={{ presentation: 'fullScreenModal' }} component={PreparingOrderScreen} />
+        <Stack.Screen name="Delivery" options={{ presentation: 'fullScreenModal' }} component={DeliveryScreen} />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
